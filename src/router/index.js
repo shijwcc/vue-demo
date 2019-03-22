9812eee5a4f0365518eb8c7a40af5f150114345b
summerdaay <459194039@qq.com>
2019-03-22 17:56:48 +0800

    nothing to say

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld'
-import testA from '@/components/testA'
-
-// import routerConfig from './routerConfig'
+import routerConfig from './routerConfig'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/testA',
-      name: 'testA',
-      component: testA
-    }
-  ]
+  routes: routerConfig
 })
-// export default new Router({
-//   routes: routerConfig
-// })
--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -1,23 +1,22 @@
-// import HelloWorld from '@/components/HelloWorld'
-// import testA from '@/components/testA'
+import HelloWorld from '@/components/HelloWorld'
+import testA from '@/components/testA'
 
-// const HelloWorld =  () => import(/* webpackChunkName: 'ImportFuncDemo' */ '@/components/HelloWorld')
 // const testA = () => import(/* webpackChunkName: 'ImportFuncDemo' */ '@/components/testA')
 
 // const HelloWorld = r =>
 //   require.ensure([], () => r(require('@/components/HelloWorld')), 'index')
 
-// const routerConfig = [
-//   {
-//     path: '/',
-//     name: 'HelloWorld',
-//     component: HelloWorld
-//   },
-//   {
-//     path: '/testA',
-//     name: 'testA',
-//     component: testA
-//   }
-// ]
-//
-// export default routerConfig
+const routerConfig = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/testA',
+    name: 'testA',
+    component: testA
+  }
+]
+
+export default routerConfig
